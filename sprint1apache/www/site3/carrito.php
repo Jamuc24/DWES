1f48990eb0ed4eaa957c467cf91401c8920a8c3a
Jamuc24 <jamuc24@fpcoruna.afundacion.org>
2025-10-07 11:19:32 +0200

    Ejercicio 4 de PHP realizado, lista de la compra , comprada?

---
+++ sprint1apache/www/site3/carrito.php
@@ -0,0 +1,34 @@
+<?php
+  
+    // Zona de declaracion del array
+
+    $carrito = array(
+        "Monster Mango" => 3.50,
+        "Bocata de chorizo" => 1.20,
+        "Danoninos pack de 4" => 1.10,
+        "Donetes de fresa" => 2.30
+    );
+
+   
+    //Zona de hacer el calculo del total del carrito
+ 
+    $compratotal = 0;
+    foreach($carrito as $precio){
+        $compratotal += $precio;
+    }
+
+    //Zona de presentar por pantalla la tabla
+
+    echo "<table border='1' cellspacing='0' cellpadding='5'>";
+    echo "<tr><th>articulo</th><th>precio (€)</th></tr>";
+
+    foreach($carrito as $articulo => $precio){
+        echo "<tr>";
+        echo "<td>$articulo</td>";
+        echo "<td>" . number_format($precio, 2) . "€</td>";
+        echo "</tr>";
+    }
+
+    echo "<tr><td><strong>TOTAL:</strong></td><td><strong>" . number_format($compratotal, 2) . "€</strong></td></tr>";
+    echo "</table>";
+?>
