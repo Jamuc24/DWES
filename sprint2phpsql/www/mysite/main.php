1bfb022e11d823a1e817172532c49dca5ff8e7ad
Jamuc24 <jamuc24@fpcoruna.afundacion.org>
2025-10-15 09:02:10 +0200

    Ejercicio 1 php-sql realizado

---
+++ sprint2phpsql/www/mysite/main.php
@@ -0,0 +1,18 @@
+
+<?php
+
+// Zona de conexion a la base de datos
+$db = mysqli_connect('localhost', 'gino', '1234', 'mysitedb') or die('No se pudo conectar a la base de datos :c');
+?>
+
+<!--Zona de confirmacion de la conexion a la base de datos-->
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Conexión php y sql</title>
+    </head>
+    <body>
+        <h1>Conexion establecida correctamente c:</h1>
+    </body>
+</html>
