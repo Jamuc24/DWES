26e4ade979bd97507f03f028d1569d6e3925a692
Jamuc24 <jamuc24@fpcoruna.afundacion.org>
2025-10-15 14:10:52 +0200

    Ejercicio 4 php-sql realizado

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -1,37 +1,62 @@
-
 <?php
-
-// Zona de conexion a la base de datos
+// Zona de conexión a la base de datos
 $db = mysqli_connect('localhost', 'gino', '1234', 'mysitedb') or die('no se pudo conectar');
 
-// Zona de comprobar que se pasa el parámetro id
+// Comprobamos que se haya pasado el id del primarca
 if (!isset($_GET['id'])) {
     die('no elegiste un primarca');
 }
 
 $id = $_GET['id'];
 
-// Zona donde se gestionan los datos de la consulta
+// Zona para incorporarlo directamente a la tabla de la base de datos
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $comentario = trim($_POST['comentario']);
+
+    if (!empty($comentario)) {
+        $query_insert = "INSERT INTO tComentarios (primarca_id, comentario) VALUES ($id, '$comentario')";
+        mysqli_query($db, $query_insert) or die('hmmmmm algo va mal');
+        header("Location: detail.php?id=$id");
+        exit;
+    }
+}
+
+// Zona de obtencion de los datos de los primarcas
 $query = "SELECT * FROM tPrimarcas WHERE id = $id";
-$result = mysqli_query($db, $query) or die('hmmmmmm algo no va bien');
+$result = mysqli_query($db, $query) or die('hmmmmm algo va mal');
 $primarca = mysqli_fetch_array($result);
 
-// Zona de salida por pantalla, primero los datos del primarca y deapues los comentarios asociados
+// Zona de para desplegar los primarcas
 echo '<h1>' . $primarca['nombre'] . '</h1>';
 echo '<h2>Legión: ' . $primarca['legion'] . '</h2>';
 echo '<img src="' . $primarca['img'] . '" alt="' . $primarca['nombre'] . '" width="200"><br><br>';
 
+// Zona para desplegar los comentarios que ya hay
 echo '<h3>Comentarios imperiales:</h3>';
 echo '<ul>';
 
 $query2 = "SELECT * FROM tComentarios WHERE primarca_id = $id";
-$result2 = mysqli_query($db, $query2) or die('hmmmmmm algo no va bien');
+$result2 = mysqli_query($db, $query2) or die('hmmmmm algo va mal');
 
-while ($comentario = mysqli_fetch_array($result2)) {
-    echo '<li>' . $comentario['comentario'] . '</li>';
+if (mysqli_num_rows($result2) > 0) {
+    while ($comentario = mysqli_fetch_array($result2)) {
+        echo '<li>' . htmlspecialchars($comentario['comentario']) . '</li>';
+    }
+} else {
+    echo '<li>Aun no hay comentarios? vaya...</li>';
 }
-
 echo '</ul>';
+
+// Zona para desplegar el formulario
+echo '
+    <hr>
+    <h3>Añadir comentario</h3>
+    <form action="detail.php?id=' . $id . '" method="POST">
+        <textarea name="comentario" rows="4" cols="50" placeholder="Escribe tu comentario..." required></textarea><br><br>
+        <input type="submit" value="Enviar comentario">
+    </form>
+';
 
 mysqli_close($db);
 ?>
