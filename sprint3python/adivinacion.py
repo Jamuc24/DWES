881e80ad37d82d64a23093d589ebcd9660092e70
Jamuc24 <jamuc24@fpcoruna.afundacion.org>
2025-11-11 13:12:46 +0100

    Ejercicio 1 iniciado

---
+++ sprint3python/adivinacion.py
@@ -0,0 +1,18 @@
+import random
+
+#Zona para declarar la variables
+
+#Zona de desplegar el juego
+print('El programa pensará un número entre 1 y un máximo que tú elijas. Intenta adivinarlo con la menor cantidad de intentos posible.')
+
+#Zona de desplegar el menú de dificultad
+print('Elije una dificultad de adivinacion')
+
+print('Facil')
+print('Medio')
+print('Dificil')
+
+#Zona de generar el numero secreto
+
+
+#Zona de 
